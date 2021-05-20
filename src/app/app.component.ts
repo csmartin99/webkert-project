17751bb69376c9a64728d0ff483716878cb0dafc
csmartin99 <csajbok.martin2@gmail.com>
2021-05-20 15:07:06 +0200

    Example test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { FirebaseService } from './firebase.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { User } from './models/user';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'webkert-project';
+  users: User[] | undefined;
+  userSub: Subscription | undefined;
+
+  constructor(private firebaseService: FirebaseService) {
+    
+  }
+
+  ngOnInit() {
+    this.getUsersByAge();
+  }
+
+  getUsersByAge() {
+    this.userSub = this.firebaseService.getUserByAge(20).subscribe(users => {
+      this.users = users;
+    });
+  }
+
+  addUser() {
+    this.firebaseService.addUser().then(data => {
+      console.log(data);
+    });
+  }
+
+  ngOnDestroy() {
+    this.userSub?.unsubscribe();
+  }
 }
--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { User } from './models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
 
-  constructor() { }
+  myUser: User = {
+    username: 'user005',
+    age: 58,
+    hometown: 'Budapest',
+    score: 43
+  };
+
+  constructor(private afs: AngularFirestore) { }
+
+  addUser() {
+    return this.afs.collection('Users').add(this.myUser);
+  }
+
+  getUserByAge(minAge: number) {
+    return this.afs.collection<User>('Users', ref => ref.where('age', '>=', minAge).orderBy('age', 'desc')).valueChanges();
+  }
 }
